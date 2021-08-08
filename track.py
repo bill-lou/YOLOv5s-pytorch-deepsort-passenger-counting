343c4f9b943639fe1494bb4d2a14ee6e9786d214
Martin Peraic <schomber@gmx.ch>
2021-08-08 02:49:11 +0200

    yapf formatting

--- track.py
+++ track.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(0, './yolov5')
 
 from yolov5.utils.datasets import LoadImages, LoadStreams
@@ -16,9 +17,7 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
-
-
-palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+palette = (2**11 - 1, 2**15 - 1, 2**20 - 1)
 
 
 def bbox_rel(*xyxy):
@@ -38,7 +37,7 @@ def compute_color_for_labels(label):
     """
     Simple function that adds fixed color depending on the class
     """
-    color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    color = [int((p * (label**2 - label + 1)) % 255) for p in palette]
     return tuple(color)
 
 
@@ -55,26 +54,30 @@ def draw_boxes(img, bbox, identities=None, offset=(0, 0)):
         label = '{}{:d}'.format("", id)
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
-        cv2.rectangle(
-            img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
-        cv2.putText(img, label, (x1, y1 +
-                                 t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+        cv2.rectangle(img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4),
+                      color, -1)
+        cv2.putText(img, label, (x1, y1 + t_size[1] + 4),
+                    cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
     return img
 
 
 def detect(opt, save_img=False):
     out, source, weights, view_img, save_txt, imgsz = \
         opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-    webcam = source == '0' or source.startswith(
-        'rtsp') or source.startswith('http') or source.endswith('.txt')
+    webcam = source == '0' or source.startswith('rtsp') or source.startswith(
+        'http') or source.endswith('.txt')
 
     # initialize deepsort
     cfg = get_config()
     cfg.merge_from_file(opt.config_deepsort)
     deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
-                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
-                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
-                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                        max_dist=cfg.DEEPSORT.MAX_DIST,
+                        min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP,
+                        max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_age=cfg.DEEPSORT.MAX_AGE,
+                        n_init=cfg.DEEPSORT.N_INIT,
+                        nn_budget=cfg.DEEPSORT.NN_BUDGET,
                         use_cuda=True)
 
     # Initialize
@@ -85,8 +88,8 @@ def detect(opt, save_img=False):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = torch.load(weights, map_location=device)[
-        'model'].float()  # load to FP32
+    model = torch.load(weights,
+                       map_location=device)['model'].float()  # load to FP32
     model.to(device).eval()
     if half:
         model.half()  # to FP16
@@ -126,8 +129,11 @@ def detect(opt, save_img=False):
         pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
-        pred = non_max_suppression(
-            pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred,
+                                   opt.conf_thres,
+                                   opt.iou_thres,
+                                   classes=opt.classes,
+                                   agnostic=opt.agnostic_nms)
         t2 = time_synchronized()
 
         # Process detections
@@ -142,8 +148,8 @@ def detect(opt, save_img=False):
 
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(
-                    img.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4],
+                                          im0.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
@@ -181,8 +187,10 @@ def detect(opt, save_img=False):
                         bbox_h = output[3]
                         identity = output[-1]
                         with open(txt_path, 'a') as f:
-                            f.write(('%g ' * 10 + '\n') % (frame_idx, identity, bbox_left,
-                                                           bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
+                            f.write(('%g ' * 10 + '\n') %
+                                    (frame_idx, identity, bbox_left, bbox_top,
+                                     bbox_w, bbox_h, -1, -1, -1,
+                                     -1))  # label format
 
             else:
                 deepsort.increment_ages()
@@ -201,17 +209,18 @@ def detect(opt, save_img=False):
                 if dataset.mode == 'images':
                     cv2.imwrite(save_path, im0)
                 else:
-                    print('saving video!')
                     if vid_path != save_path:  # new video
                         vid_path = save_path
                         if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
+                            vid_writer.release(
+                            )  # release previous video writer
 
                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                         vid_writer = cv2.VideoWriter(
-                            save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                            save_path, cv2.VideoWriter_fourcc(*opt.fourcc),
+                            fps, (w, h))
                     vid_writer.write(im0)
 
     if save_txt or save_img:
@@ -224,35 +233,55 @@ def detect(opt, save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str,
-                        default='yolov5/weights/custom_train_best.pt', help='model.pt path')
+    parser.add_argument('--weights',
+                        type=str,
+                        default='yolov5/weights/custom_train_best.pt',
+                        help='model.pt path')
     # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str,
-                        default='inference/images', help='source')
-    parser.add_argument('--output', type=str, default='out',
+    parser.add_argument('--source', type=str, default='in', help='source')
+    parser.add_argument('--output',
+                        type=str,
+                        default='out',
                         help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=640,
+    parser.add_argument('--img-size',
+                        type=int,
+                        default=640,
                         help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.4, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v',
+    parser.add_argument('--conf-thres',
+                        type=float,
+                        default=0.4,
+                        help='object confidence threshold')
+    parser.add_argument('--iou-thres',
+                        type=float,
+                        default=0.5,
+                        help='IOU threshold for NMS')
+    parser.add_argument('--fourcc',
+                        type=str,
+                        default='mp4v',
                         help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--device', default='',
+    parser.add_argument('--device',
+                        default='',
                         help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true',
+    parser.add_argument('--view-img',
+                        action='store_true',
                         help='display results')
-    parser.add_argument('--save-txt', action='store_true',
+    parser.add_argument('--save-txt',
+                        action='store_true',
                         help='save results to *.txt')
     # class 0 is person
-    parser.add_argument('--classes', nargs='+', type=int,
-                        default=[0], help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true',
+    parser.add_argument('--classes',
+                        nargs='+',
+                        type=int,
+                        default=[0],
+                        help='filter by class')
+    parser.add_argument('--agnostic-nms',
+                        action='store_true',
                         help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
+    parser.add_argument('--augment',
+                        action='store_true',
                         help='augmented inference')
-    parser.add_argument("--config_deepsort", type=str,
+    parser.add_argument("--config_deepsort",
+                        type=str,
                         default="deep_sort_pytorch/configs/deep_sort.yaml")
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
